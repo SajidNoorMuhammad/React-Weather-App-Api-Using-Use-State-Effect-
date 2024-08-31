1e5502c3a0eddaeac487cb8e95324a25069555eb
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-09-01 01:51:00 +0500

    add

--- src/Componenets/Weather.jsx
+++ src/Componenets/Weather.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import weather from "public";
 const Weather = () => {
 
     const [currentWeather, setCurrentWeather] = useState({ weather: [] });
